591d24c7d9a8758ec1328ce1023f4c7d2fda7d04
lucianobs1 <lucianotetsu@gmail.com>
2023-05-22 16:17:59 -0300

    refactor routes

--- src/routes/memories.ts
+++ src/routes/memories.ts
@@ -20,7 +20,7 @@ export async function memoriesRoutes(app: FastifyInstance) {
     return memories.map((memory) => {
       return {
         id: memory.id,
-        cover: memory.coverUrl,
+        coverUrl: memory.coverUrl,
         excerpt: memory.content.substring(0, 115).concat('...'),
       };
     });
